8e42088641972fb385e986b6696c0adeb65ca07a
selerqc <agustincharles34@gmail.com>
2025-04-06 15:50:35 +0800

    product-catalog

---
+++ product-catalog/product-catalog/Data/MongoDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using product_catalog.Model;
+
+namespace product_catalog
+{
+    public class MongoDBContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDBContext(IOptions<MongoDBSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+        public IMongoCollection<Product> Products => _database.GetCollection<Product>(nameof(Product));
+    }
+
+    public class MongoDBSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string ProductsCollectionName { get; set; }
+    }
+
+}
---
+++ product-catalog/product-catalog/Program.cs
@@ -0,0 +1,38 @@
+using MongoDB.Driver;
+using product_catalog;
+using product_catalog.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
+builder.Services.AddSingleton<MongoDBContext>();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("AllowReactApp");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
